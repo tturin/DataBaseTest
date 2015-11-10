275f3739da5729c2efcc396acd91f2ddf0fc98e2
Tim Turin <timturin@gmail.com>
2015-11-10 18:22:45 -0500

    updated MainActivity.java

--- app/src/main/java/com/tta/databasetest/MainActivity.java
+++ app/src/main/java/com/tta/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tta.databasetest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button addButton = (Button) findViewById(R.id.add);
+        Button remButton = (Button) findViewById(R.id.remove);
+        Button viewButton = (Button) findViewById(R.id.view);
+        EditText txtAdd = (EditText) findViewById(R.id.add_text);
+        EditText txtRem = (EditText) findViewById(R.id.remove_text);
+
+        addButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("You have pressed add");
+                alertDialog.setMessage("ADD MOTHERFUCKER");
+
+                alertDialog.setButton("Continue...", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //add to database
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+        remButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+            }
+        });
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +85,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Called when the user touches the add button
+    public void addToDatabase(View view)
+    {
+
     }
 }
