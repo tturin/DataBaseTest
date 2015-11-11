ab830047a06c76f23c75de871b200ab3d8cfb780
tolazhewa <tolazhewa@hotmail.com>
2015-11-11 16:10:53 -0500

    Updated MainActivity.java

--- app/src/main/java/com/tta/databasetest/MainActivity.java
+++ app/src/main/java/com/tta/databasetest/MainActivity.java
@@ -4,66 +4,95 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> names = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        File nameFile = new File("names.txt");
+        Scanner fileScanner= null;
+        final PrintWriter fileWriter;
+        try
+        {
+            fileScanner = new Scanner(nameFile);
+            fileWriter = new PrintWriter(nameFile);
+
+        }
+        catch(FileNotFoundException s)
+        {
+            System.out.println("File does Not Exist Please Try Again: ");
+        }
+
+        while(fileScanner.hasNext())
+        {
+            names.add(fileScanner.next());
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Button addButton = (Button) findViewById(R.id.add);
         Button remButton = (Button) findViewById(R.id.remove);
         Button viewButton = (Button) findViewById(R.id.view);
-        EditText txtAdd = (EditText) findViewById(R.id.add_text);
-        EditText txtRem = (EditText) findViewById(R.id.remove_text);
-
-        addButton.setOnClickListener(new View.OnClickListener()
+        final EditText txtAdd = (EditText) findViewById(R.id.add_text);
+        final EditText txtRem = (EditText) findViewById(R.id.remove_text);
+        //builder right here
+        addButton.setOnClickListener(new OnClickListener()
         {
             public void onClick(View v)
             {
-                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                alertDialog.setTitle("You have pressed add");
-                alertDialog.setMessage("ADD MOTHERFUCKER");
-
-                alertDialog.setButton("Continue...", new DialogInterface.OnClickListener() {
-                    @Override
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Add");
+                builder.setMessage("Add \"" + txtAdd.getText() + "\"?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        //add to database
+                        fileWriter.print(txtAdd.getText());
                     }
                 });
-                alertDialog.show();
-            }
-        });
-        remButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-
+                builder.setNegativeButton("No", null);
+                AlertDialog dialogAdd = builder.create();
+                dialogAdd.show();
             }
         });
 
+        remButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this); //makes the builder that formats the alert dialog
+                //builder.setTitle("Add");
+                builder.setMessage("Remove \"" + txtRem.getText() + "\"?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() //
+                {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.out.println("YES");
+                    }
+                });
+
+                builder.setNegativeButton("No", null);
+                AlertDialog dialogRem = builder.create();
+                dialogRem.show();
+            }
+        });
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,11 +114,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    //Called when the user touches the add button
-    public void addToDatabase(View view)
-    {
-
     }
 }
